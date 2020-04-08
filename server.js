207e2250deb8a12b4881f61dbc99e1fb354b32d2
tofikhidayat <topikhidayat711@gmail.com>
2020-04-09 00:42:38 +0700

    Update enviroment

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 const liveServer = require('live-server');
 const path  =  require('path');
+const env =  require('dotenv').config()
 
 
 var params = {
 	open: false,
-    port:8080, // Set the server port. Defaults to 8080.
+    port: process.env.PORT, // Set the server port. Defaults to 8080.
     host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
     root: path.normalize(__dirname+"/dist"), // Set root directory that's being served. Defaults to cwd.
     file: "404.html",
